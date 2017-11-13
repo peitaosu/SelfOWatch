ceff2bc89c12029d3fdfbf32f83c9699a3bb9338
Tony Su <peitaosu@163.com>
2017-11-13 21:59:15 +0800

    add edge detection support

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import wx
+import cv2
 
 def capture_screen(save_file):
     wx.App()
@@ -9,3 +10,9 @@ def capture_screen(save_file):
     mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
     del mem
     bmp.SaveFile(save_file, wx.BITMAP_TYPE_PNG)
+
+def canny_edge_detect(img_file, edge_file):
+    img_rgb = cv2.imread(img_file, 0)
+    img_gray = cv2.GaussianBlur(img_rgb,(3,3),0)
+    img_edge = cv2.Canny(img_gray, 50, 150)
+    cv2.imwrite(edge_file, img_edge)
